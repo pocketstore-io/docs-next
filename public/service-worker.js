9a34eb70a638740c44e9efb98d7a6dd07f87fbab
Jonathan Martz <github@jmartz.de>
2025-06-13 17:29:06 +0200

    bun update,work on sw.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,26 +3,14 @@
 self.addEventListener('install', event => {
   // Perform install steps (e.g., cache assets)
   console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    // Placeholder: Pre-cache resources here
-    Promise.resolve()
-  );
 });
 
 self.addEventListener('activate', event => {
   // Cleanup old caches, etc.
   console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    // Placeholder: Remove old caches here
-    Promise.resolve()
-  );
 });
 
 self.addEventListener('fetch', event => {
   // Intercept network requests
   console.log('[ServiceWorker] Fetch', event.request.url);
-  event.respondWith(
-    // Placeholder: Respond with cached or fetched response
-    fetch(event.request)
-  );
 });
