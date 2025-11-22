4b508c2c6f997ccd44b10d991dc57d6c274853fe
jonathan-martz <github@jmartz.de>
2025-11-22 18:29:15 +0100

    add pirsch instead of plausible,remvoe lock

--- app.vue
+++ app.vue
@@ -3,3 +3,17 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    {
+      defer: true,
+      "id": "pianjs",
+      "src": "https://api.pirsch.io/pa.js",
+      "data-code": "VdQ3pdGVifs7RYn9UH82AIjCAFcezRI6"
+    }
+  ]
+});
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
   css: ['~/tailwind.css'],
-  plausible: {
-    // Prevent tracking on localhost
-    ignoredHostnames: ['localhost'],
-  },
   vite: {
     plugins: [
       tailwindcss(),
