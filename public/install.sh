b2364882934d44e1149252ba526bdda1cdbfa62f
jonathan-martz <github@jmartz.de>
2025-12-07 12:32:47 +0100

    combine install.sh nvm commands

--- public/install.sh
+++ public/install.sh
@@ -36,9 +36,7 @@ go run bin/custom.go
 go run bin/plugins.go
 go run bin/translations.go
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source ~/.bashrc
-nvm install --lts
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash && source ~/.bashrc && nvm install --lts
 npm i -g npm bun pm2
 
 cd storefront && bun install && go run bin/sitemap.go && bun run build
